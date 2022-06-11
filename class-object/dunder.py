704447526cb7eeda86329ad226cc3937d3470ebe
Javohir.py <97449821+Javohir-dev@users.noreply.github.com>
2022-06-11 22:42:00 +0500

    Added new lesson code

--- class-object/dunder.py
+++ class-object/dunder.py
@@ -1,3 +1,6 @@
+# dars linki: https://www.youtube.com/watch?v=KOKEsau2uaU&t=0s
+# ? 1-qism
+
 class Avto:
     """Avtomobil klassi"""
     __num_avto = 0
@@ -49,8 +52,8 @@ avto2 = Avto("GM", "Malibu", "Qora", 2020, 20000)
 # print(avto1 <= avto2)
 
 
-class AvtoSalom:
-    """Avtosalomm classi"""
+class AvtoSalon:
+    """Avtosalonm classi"""
 
     def __init__(self, name):
         self.name = name
@@ -69,7 +72,7 @@ class AvtoSalom:
         """
         return self.avtolar[index]
 
-    def __getitem__(self, index, qiymat):
+    def __setitem__(self, index, qiymat):
         """
         Endi bemalol royxat ichidagi elementlarni 
         boshqasi bilan index orqali almashtirishimiz ham mumkin.
@@ -77,6 +80,27 @@ class AvtoSalom:
         shu yo'ldan foydalanamiz
         """
         self.avtolar[index] = qiymat
+
+    def __call__(self, *qiymat):
+        if qiymat:
+            for avto in qiymat:
+                self.add_avto(avto)
+        else:
+            return [avto for avto in self.avtolar]
+
+    def __add__(self, y):
+        """
+        Ikkala salon nomi va mashinalarni
+        bir biriga qoshiadigan method
+        """
+        if isinstance(y, AvtoSalon):
+            yangi_salon = AvtoSalon(f"{self.name} {y.name}")
+            yangi_salon.avtolar = self.avtolar + y.avtolar
+            return yangi_salon
+        elif isinstance(y, Avto):
+            """agar user avto kiritsa yani salon1 + avto4 
+            salon1 ga yangi mashina qoshiladi"""
+            self.add_avto(y)
 
     def add_avto(self, *qiymat):
         for avto in qiymat:
@@ -86,14 +110,28 @@ class AvtoSalom:
                 print("Avto kiriting")
 
 
-salon1 = AvtoSalom("MaxAvto")
+# ? Bu bilan yangi salon yaratib unga nom berayapmiz.
+salon1 = AvtoSalon("MaxAvto")
+
+# ? Yangi salon yaratildi
+salon2 = AvtoSalon("Avto Lider")
 
 avto1 = Avto("GM", "Malibu", "Qora", 2020, 40000)
 avto2 = Avto("GM", "Lacetti", "Oq", 2020, 20000)
 avto3 = Avto("Toyota", 'Carolla', "Silver", 2018, 45000)
 salon1.add_avto(avto1, avto2, avto3)
-# avto4 = Avto("Mazda", "6", 'Qizil', 2015, 35000)
-# avto5 = Avto("Volkswagen", "Polo", 'Qora', 2015, 30000)
-# avto6 = Avto("Honda", "Accord", "Oq", 2017, 42000)
 
-print(salon1[0])
+avto4 = Avto("Mazda", "6", 'Qizil', 2015, 35000)
+avto5 = Avto("Volkswagen", "Polo", 'Qora', 2015, 30000)
+avto6 = Avto("Honda", "Accord", "Oq", 2017, 42000)
+salon2(avto4, avto5, avto6)
+
+
+salon3 = salon1 + salon2
+# print(f"\n{salon3()}\n")
+salon1 + avto4  # orqali yangi mashina qoshayapmiz
+print(salon1())
+
+
+# dars linki: https://youtu.be/ltTOXJJiPRk
+# ? 2-qism
