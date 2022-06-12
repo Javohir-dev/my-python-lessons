78017c695175c97de9a871e4e65c0db812d7a10c
Javohir.py <97449821+Javohir-dev@users.noreply.github.com>
2022-06-12 14:31:01 +0500

    Added new file file.py and pi.txt

---
+++ work_with_files/file.py
@@ -0,0 +1,8 @@
+fayl = open('pi.txt')
+PI = fayl.read()
+print(PI)
+fayl.close()
+
+# with open('pi.txt') as fayl:
+#     pi = fayl.read()
+# ? eng avzal yo'li shu with bilanfileni oqib bo'lin yopib qoyadi
